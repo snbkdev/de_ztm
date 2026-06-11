6cc11c162bfbf446aa43a3f1ca6a3df02f92a445
Asanbek09 <samudinua@gmail.com>
2026-06-11 17:31:01 +0600

    Подключение к postgres и чтение данных

--- Section02/apache_airflow/dags/02_bookings_per_listing.py
+++ Section02/apache_airflow/dags/02_bookings_per_listing.py
@@ -1,45 +1,72 @@
-from airflow import DAG
+from airflow import DAG, task
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import get_current_context
 import random
 import os, csv
+from datetime import datetime, timedelta
 
 DAG_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-def generate_bookings(**context):
+
+def read_bookings_from_postgres():
+    context = get_current_context()
     execution_date = context["data_interval_start"]
-    ds = context["ds"]
-    time_str = execution_date.strftime("%Y-%m-%d_%H%M")
     
-    file_path = ""
-    
-    num_bookings = random.randint(30, 50)
+    file_date = execution_date.strftime("%Y-%m-%d_%H%M")
+    file_path = f""
+
+    start_of_minute = execution_date.replace(second=0, microsecond=0)
+    end_of_minute = start_of_minute + timedelta(minutes=1)
+
+    start_of_minute_plus_6 = start_of_minute + timedelta(hours=6)
+    end_of_minute_plus_6 = end_of_minute + timedelta(hours=6)
+
+    pg_hook = PostgresHook(postgres_conn_id="postgres_bookings")
+    query = f"""
+        select booking_id, listing_id, user_id, booking_time, status from bookings
+        where booking_time >= '{start_of_minute_plus_6.strftime('%Y-%m-%d %H:%M:%S')}'
+        and booking_time < '{end_of_minute_plus_6.strftime('%Y-%m-%d %H:%M:%S')}'
+        """
+
+    records = pg_hook.get_records(query)
+
     bookings = []
-    for _ in range(num_bookings):
+
+    print(f"Read {len(records)} from Postgres")
+    for record in records:
         booking = {
-            "booking_id": random.randint(1000, 5000),
-            "listing_id": random.choice([13913, 17402, 24328, 33332, 116268, 117203, 127652, 127860]),
-            "user_id": random.randint(1000, 5000),
-            "booking_time": time_str,
-            "status": random.choice(["confirmed", "cancelled", "pending"])
+            "booking_id": record[0],
+            "listing_id": record[1],
+            "user_id": record[2],
+            "booking_time": record[3].strftime('%Y-%m-%d %H:%M:%S'),
+            "status": record[4]
         }
         bookings.append(booking)
-    
+
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    
+
     fieldnames = ["booking_id", "listing_id", "user_id", "booking_time", "status"]
-    
+
     with open(file_path, "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for booking in bookings:
-            writer.writerow(booking)
-    
-    print(f"generated bookings data written to {file_path}")
+            writer.writerow({
+                "booking_id": booking["booking_id"],
+                "listing_id": booking["listing_id"],
+                "user_id": booking["user_id"],
+                "booking_time": booking["booking_time"],
+                "status": booking["status"],
+            })
+
+    print(f"Generated bookings data written to {file_path}")
+
     return file_path
 
 default_args = {
@@ -55,9 +82,9 @@ with DAG(
     description='Process bookings with Spark',
 ) as dag:
     
-    generate_bookings_task = PythonOperator(
-        task_id='generate_bookings',
-        python_callable=generate_bookings,
+    read_bookings_task = PythonOperator(
+        task_id='read_bookings_from_postgres',
+        python_callable=read_bookings_from_postgres,
     )
 
     wait_for_listings_file = FileSensor(
@@ -79,9 +106,7 @@ with DAG(
         ],
         conn_id='spark_booking',
     )
-    
-    # generate_bookings_task >> spark_job
-    # wait_for_listings_file >> spark_job
 
-    # generate_bookings_task >> wait_for_listings_file >> spark_job
-    [generate_bookings_task, wait_for_listings_file] >> spark_job
+
+    # bookings_file = read_bookings_from_postgres()
+    [read_bookings_task, wait_for_listings_file] >> spark_job
